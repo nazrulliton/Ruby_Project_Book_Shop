79fa97c81e55c7b4f45fab9ceebf525a6107bd51
naz_liton <nazrulliton@icloud.com>
2019-11-10 15:45:13 +0000

    Suppler class/model created

---
+++ models/supplier.rb
@@ -0,0 +1,13 @@
+class Stock
+attr_accessor :id, :name, :address
+
+def initialize(options)
+  @id = options['id'].to_i if options['id']
+  @name = options['name']
+  @address = options['address']
+  
+
+end
+
+
+end
