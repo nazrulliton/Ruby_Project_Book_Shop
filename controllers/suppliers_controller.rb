4ae12b2489f0ff7a33e422790df4742caf3c3a8a
naz_liton <nazrulliton@icloud.com>
2019-11-13 11:08:14 +0000

    edit button and path added to supplier

--- controllers/suppliers_controller.rb
+++ controllers/suppliers_controller.rb
@@ -14,17 +14,31 @@ get '/suppliers/new' do
   erb (:"suppliers/new")
 end
 
+post '/suppliers' do
+  supplier = Supplier.new(params)
+  supplier.save()
+  redirect to ('/suppliers')
+end
+
 get '/suppliers/:id' do
   @supplier = Supplier.find_by_id(params['id'].to_i)
   @books =  @supplier.books()
   erb(:"suppliers/show")
 end
 
-post '/suppliers' do
-  supplier = Supplier.new(params)
-  supplier.save()
-  redirect to ('/suppliers')
+get '/suppliers/:id/edit' do
+  @supplier = Supplier.find_by_id(params['id'])
+  @books = Book.all
+  @stock = Stock.all
+  erb(:"suppliers/edit")
 end
+
+post '/suppliers/:id' do
+  supplier = Supplier.new(params)
+  supplier.update
+  redirect to "/supplier/#{params['id']}"
+end
+
 
 post '/suppliers/:id/delete' do
   supplier = Supplier.find_by_id(params['id'])
